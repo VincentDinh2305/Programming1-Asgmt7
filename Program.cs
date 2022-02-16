b3272af3ca59d580a2294e0a2192eaf72d369e84
VincentDinh2305 <90953477+VincentDinh2305@users.noreply.github.com>
2022-02-16 05:51:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = "the quick brown fox jumps over the lazy dog".ToCharArray();
+            int[] numbers = { 0, 2, 3, 5, 7, 1, 1, 2, 5, 6, 7, 2, 5, 2 };
+            string[] poem = "mary had a little lamb its fleece was white as snow".Split();
+            Array.Reverse(numbers);
+            FourOne(numbers);            
+        }
+
+        static void FourOne(int[] toFour)
+        {
+            foreach (var x in toFour)
+            {
+                Console.Write($"{x}  ");
+            }
+        }      
+
+    }
+}
